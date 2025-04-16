3092c842cff57c7d664dacb55f37af3a6ad3b298
wwmarkositrawe <wungielq@gmail.com>
2025-04-16 12:28:46 +0200

    dorzutka

--- docs/game.js
+++ docs/game.js
@@ -1,33 +1,85 @@
-// Prosta gra Phaser.js
+let cursors;
+let grafika;
+let dzwiek1, dzwiek2;
+let tekstStart;
+let tekstRestart;
+let konfetti = [];
+
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
     scene: {
         preload: preload,
         create: create,
         update: update
+    },
+    audio: {
+        disableWebAudio: false
     }
 };
 
-let player;
+const game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('player', 'player.png'); // Załaduj grafikę dla gracza
+    this.load.image('grafika', 'grafika.jpg');
+    this.load.audio('dzwiek1', 'nagranie1.mp3');
+    this.load.audio('dzwiek2', 'nagranie2.mp3');
 }
 
 function create() {
-    player = this.add.sprite(400, 300, 'player'); // Stwórz gracza na ekranie
+    grafika = this.add.image(300, 200, 'grafika').setDisplaySize(600, 400);
+    dzwiek1 = this.sound.add('dzwiek1');
+    dzwiek2 = this.sound.add('dzwiek2');
+
+    tekstStart = this.add.text(100, 180, "Kliknij, by zacząć", {
+        fontSize: '32px',
+        fill: '#000'
+    });
+
+    this.input.once('pointerdown', () => {
+        tekstStart.setVisible(false);
+        startGame.call(this);
+    });
+}
+
+function startGame() {
+    const powt = Phaser.Math.Between(1, 5);
+    const odtworz = (n) => {
+        if (n > 0) {
+            dzwiek1.play();
+            dzwiek1.once('complete', () => odtworz(n - 1));
+        } else {
+            dzwiek2.play();
+            dzwiek2.once('complete', () => pokazKonfetti.call(this));
+        }
+    };
+    odtworz(powt);
+}
+
+function pokazKonfetti() {
+    for (let i = 0; i < 100; i++) {
+        konfetti.push({
+            x: Phaser.Math.Between(0, 600),
+            y: Phaser.Math.Between(0, 400),
+            r: Phaser.Math.Between(3, 7),
+            kolor: Phaser.Display.Color.RandomRGB()
+        });
+    }
+    tekstRestart = this.add.text(100, 180, "Kliknij, by zagrać jeszcze raz", {
+        fontSize: '28px',
+        fill: '#ff0000'
+    });
+    this.input.once('pointerdown', () => location.reload());
 }
 
 function update() {
-    // Prosta logika do poruszania graczem
-    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.LEFT)) {
-        player.x -= 5;
-    }
-    if (this.input.keyboard.isDown(Phaser.Input.Keyboard.KeyCodes.RIGHT)) {
-        player.x += 5;
+    const g = game.scene.scenes[0];
+    if (konfetti.length > 0) {
+        konfetti.forEach(k => {
+            const kolo = g.add.circle(k.x, k.y, k.r, k.kolor.color);
+            setTimeout(() => kolo.destroy(), 1000);
+        });
+        konfetti = [];
     }
 }
-
-const game = new Phaser.Game(config);
